5fd47ccf55b1c858177ecd2267024738013a75a5
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-10-04 16:25:57 +0200

    Add `scrollTopOnChange` attr. Fix syntax.

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -1,30 +1,36 @@
 angular.module('perfect_scrollbar', [])
-    .directive('perfectScrollbar', function ($parse, $timeout) {
-        return {
-            restrict: 'E',
-            transclude: true,
-            template:  '<div><div ng-transclude></div></div>',
-            replace: true,
-            link: function ($scope, $elem, $attr) {
-                $elem.perfectScrollbar({
-                    wheelSpeed: $parse($attr.wheelSpeed)() || 50,
-                    wheelPropagation: $parse($attr.wheelPropagation)() || false,
-                    minScrollbarLength: $parse($attr.minScrollbarLength)() || false,
-                });
+.directive('perfectScrollbar', function ($parse, $timeout) {
+  return {
+    restrict: 'E',
+    transclude: true,
+    template:  '<div><div ng-transclude></div></div>',
+    replace: true,
+    link: function ($scope, $elem, $attr) {
+      $elem.perfectScrollbar({
+        wheelSpeed: $parse($attr.wheelSpeed)() || 50,
+        wheelPropagation: $parse($attr.wheelPropagation)() || false,
+        minScrollbarLength: $parse($attr.minScrollbarLength)() || false,
+      });
 
-                if ($attr.refreshOnChange) {
-                    updatePerfectScrollbar = function() {
-                        $timeout(function () {
-                            $elem.perfectScrollbar('update');
-                        }, 10);
-                    };
+      updatePerfectScrollbar = function() {
+        $timeout(function () {
+          $elem.perfectScrollbar('update');
+        }, 10);
+      };
 
-                    if (typeof $scope.$eval($attr.refreshOnChange) == 'object') {
-                        $scope.$watchCollection($attr.refreshOnChange, updatePerfectScrollbar);
-                    } else {
-                        $scope.$watch($attr.refreshOnChange, updatePerfectScrollbar);
-                    }
-                }
-            }
+      if ($attr.refreshOnChange) {
+        if (typeof $scope.$eval($attr.refreshOnChange) == 'object') {
+          $scope.$watchCollection($attr.refreshOnChange, updatePerfectScrollbar);
+        } else {
+          $scope.$watch($attr.refreshOnChange, updatePerfectScrollbar);
         }
-    });
+      }
+
+      if ($attr.scrollTopOnChange) {
+        $scope.$watch($attr.scrollTopOnChange, function(){
+          $elem.scrollTop(0);
+        });
+      }
+    }
+  };
+});
