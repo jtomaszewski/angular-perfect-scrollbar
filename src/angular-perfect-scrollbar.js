bc5ad155f1c4bf288e4c1bf6235300798cc5c63c
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2013-10-06 14:45:23 +0200

    Allow to set global options with provider.

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -1,16 +1,57 @@
 angular.module('perfect_scrollbar', [])
-.directive('perfectScrollbar', function ($parse, $timeout) {
+
+.provider('perfectScrollbar', function(){
+  _this = this;
+
+  _this.options = {};
+  possibleOptions = ['wheelSpeed', 'wheelPropagation', 'minScrollbarLength', 'useBothWheelAxes', 'useKeyboard'];
+
+  _this.setOptions = function(options) {
+    for (var i in possibleOptions) {
+      key = possibleOptions[i];
+      if (typeof options[key] !== 'undefined') {
+        _this.options[key] = options[key];
+      }
+    }
+
+    return _this;
+  };
+
+  _this.$get = function(){
+    return {
+      possibleOptions: possibleOptions,
+
+      getOptions: function(){
+        return _this.options;
+      }
+    };
+  };
+})
+
+.directive('perfectScrollbar', ['$parse', '$timeout', 'perfectScrollbar', function ($parse, $timeout, perfectScrollbar) {
   return {
     restrict: 'E',
     transclude: true,
     template:  '<div><div ng-transclude></div></div>',
     replace: true,
     link: function ($scope, $elem, $attr) {
-      $elem.perfectScrollbar({
-        wheelSpeed: $parse($attr.wheelSpeed)() || 50,
-        wheelPropagation: $parse($attr.wheelPropagation)() || false,
-        minScrollbarLength: $parse($attr.minScrollbarLength)() || false,
-      });
+      // You can set perfect-scrollbar options by setting an attribute on
+      // directive's element (f.e. `wheel-propagation="false"`);
+      // or setting it globally with `perfectScrollbarProvider.setOptions`.
+      var getOptions = function() {
+        options = perfectScrollbar.getOptions();
+
+        for (var i in perfectScrollbar.possibleOptions) {
+          key = perfectScrollbar.possibleOptions[i];
+          if ($attr[key]) {
+            options[key] = $parse($attr[key])();
+          }
+        }
+
+        return options;
+      };
+
+      $elem.perfectScrollbar(getOptions());
 
       updatePerfectScrollbar = function() {
         $timeout(function () {
@@ -18,6 +59,7 @@ angular.module('perfect_scrollbar', [])
         }, 10);
       };
 
+      // Update perfect-scrollbar when `refresh-on-change` attr changes.
       if ($attr.refreshOnChange) {
         if (typeof $scope.$eval($attr.refreshOnChange) == 'object') {
           $scope.$watchCollection($attr.refreshOnChange, updatePerfectScrollbar);
@@ -26,6 +68,7 @@ angular.module('perfect_scrollbar', [])
         }
       }
 
+      // Scroll container to top when `scroll-top-on-change` attr changes.
       if ($attr.scrollTopOnChange) {
         $scope.$watch($attr.scrollTopOnChange, function(){
           $elem.scrollTop(0);
@@ -33,4 +76,4 @@ angular.module('perfect_scrollbar', [])
       }
     }
   };
-});
+}]);
